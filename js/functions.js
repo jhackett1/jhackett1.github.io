b0f36940b5dfbe297432b0fa9ba1569441e72c96
Joshua Hackett <hello@joshuahackett.com>
2017-05-31 22:43:57 +0100

    Add lazy loading functionality

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,56 @@
 $(document).ready(function() {
 
+  // Function to lazily load in alternate images on home page slider
+  function lazyHero(){
+    document.querySelectorAll('section.hero figure.slideshow div').forEach(function(slide, iterator){
+      // Don't lazily load the first slide, only later ones
+      if (iterator < 3) {
+        // Create the delay based on current loop iteration
+        setTimeout(function(){
+          // Do the magic
+          slide.style.backgroundImage = "url("+ slide.getAttribute('data-image-src') +")";
+        }, iterator*1000)
+      }
+    });
+  }
 
-
-
-
-
-
-  // Check if the featured image container is in view, and if not, make the next-previous navigation visible
-  $(window).scroll(function() {
-    // Calculate vars
-    var bottom_of_hero = $(".featured-image").offset().top + $(".featured-image").outerHeight();
-    var top_of_screen = $(window).scrollTop();
-    // Act on vars
-    if(bottom_of_hero < top_of_screen){
-        $(".previous-next").addClass("visible");
-    } else{
-        $(".previous-next").removeClass("visible");
+  // Animate in next/prev thingy
+  function nextPrevHandler(){
+    // Grab the elements
+    var element = document.querySelector('.featured-image');
+    var nextPrev = document.querySelector('.previous-next');
+    // If element is off the top of the viewport
+    if (element.getBoundingClientRect().bottom < 0) {
+      nextPrev.classList.add('visible');
+    } else {
+      nextPrev.classList.remove('visible');
     }
-  });
+  }
+
+  // Function to load in images
+  function lazyLoadHandler(){
+    // Lazy-loading images
+    document.querySelectorAll('article.category-tile div.image-holder').forEach(function(element){
+      // If the image is in view, load it in
+      if (element.getBoundingClientRect().top > 0 && document.documentElement.clientHeight > element.getBoundingClientRect().top ) {
+        // Do the magic
+        element.style.backgroundImage = "url("+ element.getAttribute('data-image-src') +")";
+        // Add the class to fade in the newly loaded image
+        if (element.classList == "image-holder") {
+          element.classList.add('loaded');
+        }
+      }
+    })
+  }
+
+  // Initial actions on page load
+  lazyLoadHandler();
+  lazyHero();
+
+  // // Scroll event handler for multiple functions
+  window.addEventListener('scroll', function(){
+    lazyLoadHandler();
+    nextPrevHandler();
+  })
 
 });
